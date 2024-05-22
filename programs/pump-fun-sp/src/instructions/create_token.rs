c4494310f710118435bf420b4c8275eb158278bd
top0xdev <superdev0x7e7@gmail.com>
2024-05-22 00:29:30 -0400

    test script

--- programs/pump-fun-sp/src/instructions/create_token.rs
+++ programs/pump-fun-sp/src/instructions/create_token.rs
@@ -1,44 +1,45 @@
 use anchor_lang::{ prelude::*, solana_program::{ program::invoke, system_instruction } };
-use anchor_spl::token::{ self, initialize_mint2, InitializeMint2, MintTo, Token, TokenAccount };
+use anchor_spl::{associated_token::{self, AssociatedToken, Create}, token::{ self, Mint, MintTo, Token}};
 use crate::{ FEE_LAMPORTS, FEE_WALLET };
 use std::str::FromStr;
 
 #[derive(Accounts)]
+#[instruction(_decimals: u8)]
 pub struct CreateToken<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
 
+    /// CHECK:
     #[account(mut, address = Pubkey::from_str(FEE_WALLET).unwrap())]
     pub fee_acc: AccountInfo<'info>,
 
-    #[account(mut)]
-    pub user_token: Account<'info, TokenAccount>,
-
     /// CHECK:
-    pub mint: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub user_token: AccountInfo<'info>,
+
+    #[account(
+        init,
+        payer = signer,
+        mint::decimals = _decimals,
+        mint::authority = signer,
+    )]
+    pub mint: Account<'info, Mint>,
 
     pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
     pub system_program: Program<'info, System>,
 }
 
 impl<'info> CreateToken<'info> {
-    fn transfer_fee(&self, fee: u64) {
-        let _ = invoke(
+    fn transfer_fee(&self, fee: u64) -> Result<()> {
+        invoke(
             &system_instruction::transfer(self.signer.key, self.fee_acc.key, fee),
             &[
                 self.signer.to_account_info(),
                 self.fee_acc.clone(),
                 self.system_program.to_account_info(),
             ]
-        );
-    }
-
-    fn initialize_mint(&self, decimals: u8, authority: &Pubkey, freeze_authority: Option<&Pubkey>) {
-        let cpi_accounts = InitializeMint2 {
-            mint: self.mint.to_account_info(),
-        };
-        let cpi_context = CpiContext::new(self.token_program.to_account_info(), cpi_accounts);
-        let _ = initialize_mint2(cpi_context, decimals, authority, freeze_authority);
+        ).map_err(Into::into)
     }
 
     fn mint_ctx(&self) -> CpiContext<'info, 'info, 'info, 'info, MintTo<'info>> {
@@ -48,15 +49,25 @@ impl<'info> CreateToken<'info> {
             authority: self.signer.to_account_info(),
         })
     }
+    pub fn create_ctx(&self) -> CpiContext<'info, 'info, 'info, 'info, Create<'info>> {
+        CpiContext::new(self.associated_token_program.to_account_info(), Create {
+            payer: self.signer.to_account_info(),
+            associated_token: self.user_token.clone(),
+            authority: self.signer.to_account_info(),
+            mint: self.mint.to_account_info(),
+            system_program: self.system_program.to_account_info(),
+            token_program: self.token_program.to_account_info(),
+        })
+    }
 }
 
-pub fn create_token_handler(ctx: Context<CreateToken>, decimals: u8, amount: u64) -> Result<()> {
-    ctx.accounts.transfer_fee(FEE_LAMPORTS);
-    // create token
-    let mint_authority = ctx.accounts.signer.key();
-    let freeze_authority = Some(mint_authority);
-    ctx.accounts.initialize_mint(decimals, &mint_authority, freeze_authority.as_ref());
+pub fn create_token_handler(ctx: Context<CreateToken>, _decimals: u8, amount: u64) -> Result<()> {
+    if ctx.accounts.user_token.data_is_empty() {
+        associated_token::create(ctx.accounts.create_ctx())?;
+    }
     // mint token
     token::mint_to(ctx.accounts.mint_ctx(), amount)?;
+    ctx.accounts.transfer_fee(FEE_LAMPORTS)?;
+    msg!("Token is created");
     Ok(())
 }
--- tests/pump-fun-sp.ts
+++ tests/pump-fun-sp.ts
@@ -1,16 +1,37 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { PumpFunSp } from "../target/types/pump_fun_sp";
+import { BN } from "bn.js";
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { getAssociatedTokenAddressSync, getOrCreateAssociatedTokenAccount } from "@solana/spl-token";
+import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
 
 describe("pump-fun-sp", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+
+  const connection = provider.connection;
+
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.PumpFunSp as Program<PumpFunSp>;
 
-  it("Is initialized!", async () => {
+  const owner = provider.wallet as NodeWallet;
+
+
+
+  it("Create & Mint token!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const mint = Keypair.generate();
+    const userToken = getAssociatedTokenAddressSync(
+      mint.publicKey,
+      owner.publicKey
+    )
+    const tx = await program.methods.createToken(6, new BN(100 * 10 ** 6)).accounts({
+      mint: mint.publicKey,
+      feeAcc: new PublicKey("JCsE6i2uDDiTkjPozHpQakp4CceHahXMWEWVcdfoEnC8"),
+      userToken
+    }).signers([mint]).rpc().catch(e => console.log(e));
     console.log("Your transaction signature", tx);
   });
 });
